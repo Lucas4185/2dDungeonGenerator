5625d1271e55068d0ae294494453435bbb8a00e8
Lucas4185 <30445090+Lucas4185@users.noreply.github.com>
2019-11-21 11:18:46 +0100

    Update

--- 2dDungeonGenerator/Assets/Scripts/RoomGenerator.cs
+++ 2dDungeonGenerator/Assets/Scripts/RoomGenerator.cs
@@ -4,15 +4,63 @@ using UnityEngine;
 
 public class RoomGenerator : MonoBehaviour
 {
+    [SerializeField]
+    private int openingDirection;
+    private int rand;
+
+    private RoomTemplates templates;
+
+    [SerializeField]
+    private bool spawned = false;
     // Start is called before the first frame update
     void Start()
     {
-        
+        templates = GameObject.FindGameObjectWithTag("Rooms").GetComponent<RoomTemplates>();
+        Invoke("Spawn" , 0.1f);
     }
 
     // Update is called once per frame
-    void Update()
+    void Spawn()
     {
+        if(spawned == false)
+        {
+            if (openingDirection == 1)
+            {
+                rand = Random.Range(0, templates.bottomRooms.Length);
+                Instantiate(templates.bottomRooms[rand], transform.position, templates.bottomRooms[rand].transform.rotation);
+               
+            }
+            else if (openingDirection == 2)
+            {
+                rand = Random.Range(0, templates.topRooms.Length);
+                Instantiate(templates.topRooms[rand], transform.position, templates.topRooms[rand].transform.rotation);
+                
+            }
+            else if (openingDirection == 3)
+            {
+                rand = Random.Range(0, templates.leftRooms.Length);
+                Instantiate(templates.leftRooms[rand], transform.position, templates.leftRooms[rand].transform.rotation);
+                
+            }
+            else if (openingDirection == 4)
+            {
+                rand = Random.Range(0, templates.rightRooms.Length);
+                Instantiate(templates.rightRooms[rand], transform.position, templates.rightRooms[rand].transform.rotation);
+                
+            }
+            spawned = true;
+        }
         
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("SpawnPoint"))
+            if (other.GetComponent<RoomGenerator>().spawned == false && spawned == false)
+        {
+                Instantiate(templates.closedroom, transform.position, Quaternion.identity);
+                Destroy(gameObject);
+        }
+        spawned = true;
     }
 }
